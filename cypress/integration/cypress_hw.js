4af7526174bcde5bb8e062641e4ac647e9a6d4a3
Irina1302 <imikhailenko92@gmail.com>
2021-02-09 18:21:42 +0200

    cypress_hw1

---
+++ cypress/integration/cypress_hw.js
@@ -0,0 +1,13 @@
+
+it ('Signin flow', () =>{
+  cy.visit('https://react-redux.realworld.io/#/login')
+
+  cy.get(':nth-child(1) > .form-control').type('testest1@gmail.com')
+  cy.get(':nth-child(2) > .form-control').type('123456789')
+
+  cy.get('.btn').click()
+
+  cy.get('.navbar').contains('testest11').should('exist')
+  cy.get();
+
+})
